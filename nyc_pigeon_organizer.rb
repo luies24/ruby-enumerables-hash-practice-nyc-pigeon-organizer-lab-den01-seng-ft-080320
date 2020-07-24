4da7f5e76ece98a792bbe1b00f1bba3e6325440e
Luis Garcia <l.garcia24@hotmail.com>
2020-07-24 01:35:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  data.count do |key, value1|
+  data.each_with_object({}) do |key, value, finally_arr|
     if key == data[:gender]
       data[:gender].count do |key, value2|
         organized_hash = {value2 => ""}
