ce72c00c2fae10faff9695e47546c6e46727792f
Luis Garcia <l.garcia24@hotmail.com>
2020-07-24 01:38:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |key, value, finally_arr|
     value.each do |inn_key, names|
       names.each do |name|
-        
+        if !finally_arr
       end
     end
   end
